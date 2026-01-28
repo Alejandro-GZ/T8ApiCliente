f76c89d1c1dcc9f05bc561592a65f05640cc53e2
Alejandro-GZ <alejandrogonzalezzapico@gmail.com>
2026-01-28 13:08:23 +0000

    Computación del espectro

--- src/t8-client/models/waveform.py
+++ src/t8-client/models/waveform.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 import numpy as np
 from basedata import BaseData
+from numpy.fft import fft, fftfreq
 
 from ..utils.plotting import plot_xy
 
@@ -23,5 +24,31 @@ class WaveformData(BaseData):
             title=f"Waveform for signal path: {self.path}",
             output_path=output_path
         )
-    
-    #TODO: Calc spectra
+
+    def compute_spectrum(self, fmin: float, fmax: float) \
+        -> tuple[np.ndarray, np.ndarray]: #TODO: Retrieve fmin and fmax from config
+        """
+        Compute the frequency spectrum of a given waveform within a specified frequency
+        range.
+
+        Parameters:
+        waveform: The input signal waveform.
+        sample_rate: The sampling rate of the waveform in Hz.
+        fmin: The minimum frequency of interest in Hz.
+        fmax: The maximum frequency of interest in Hz.
+
+        Returns:
+        A tuple containing:
+            - filtered_freqs: The corresponding frequencies within the
+                specified range.
+            - filtered_spectrum: The magnitude of the frequency spectrum within
+                the specified range, with an RMS AC detector.
+        """
+        waveform = self.decode()
+        sample_rate = self.sample_rate
+        spectrum = fft(waveform) * 2 * np.sqrt(2)
+        magintude = np.abs(spectrum) / len(spectrum)
+        freqs = fftfreq(len(waveform), 1 / sample_rate)
+        filtered_spectrum = magintude[(freqs >= fmin) & (freqs <= fmax)]
+        filtered_freqs = freqs[(freqs >= fmin) & (freqs <= fmax)]
+        return filtered_freqs, filtered_spectrum
